1aead313fc78020857c5286dd225d771a593b137
yy <96430694+xiaojingyy@users.noreply.github.com>
2022-02-15 17:43:26 +0800

    Add files via upload

---
+++ 运算符重载/05关系运算符重载.cpp
@@ -0,0 +1,49 @@
+//#include<iostream>
+//#include<iomanip>
+//#include<string>
+//using namespace std;
+//
+//class person
+//{
+//public:
+//	person(string name, int age)
+//	{
+//		m_name = name;
+//		m_age = age;
+//	}
+//
+//	bool operator==(person& p)
+//	{
+//		if (this->m_age == p.m_age && this->m_name == p.m_name)
+//		{
+//			return true;
+//		}
+//	}
+//	string m_name;
+//	int m_age;
+//};
+//
+//void test01()
+//{
+//	person p1("tom", 18);
+//	person p2("tom", 18);
+//
+//	if (p1 == p2)
+//	{
+//		cout << "p1和p2是相等的" << endl;
+//	}
+//	else
+//	{
+//		cout << "p1和p2不相等" << endl;
+//	}
+//	
+//}
+//
+//
+//int main()
+//{
+//	test01();
+//
+//	system("pause");
+//	return 0;
+//}
